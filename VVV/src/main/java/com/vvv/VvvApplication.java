fa2c2f3a2ec571c775b96a660afbffedabb2752c
João Ramos <joaoramosreis24@gmail.com>
2023-03-12 15:51:52 -0300

    Projeto VVV
    Subindo o projeto pelo GitHub Desktop

---
+++ VVV/src/main/java/com/vvv/VvvApplication.java
@@ -0,0 +1,13 @@
+package com.vvv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VvvApplication.class, args);
+	}
+
+}
